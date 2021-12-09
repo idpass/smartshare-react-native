3d5d7a501181d8c2b5e652b8a7f80a7c5d7b70a2
dexter <typelogic@pm.me>
2021-12-09 23:44:10 +0800

    Update idpass-smartshare.aar and switch over to new package name

--- android/src/main/java/com/reactnativeidpasssmartshare/BluetoothApi.java
+++ android/src/main/java/com/reactnativeidpasssmartshare/BluetoothApi.java
@@ -29,7 +29,7 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.bridge.Callback;
 
-import org.idpass.smartshare.bluetooth.BluetoothSecure;
+import org.idpass.smartshare.connection.BluetoothSecure;
 import org.json.JSONException;
 import org.json.JSONObject;
 
