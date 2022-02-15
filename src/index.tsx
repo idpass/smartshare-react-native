0352ac5317f84e21f532f1931aa386671892e619
Arnelle Balane <arnelle@newlogic.com>
2022-02-15 12:07:10 +0800

    Rename package to @idpass/smartshare-react-native

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@
 import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
 
 const LINKING_ERROR =
-  `The package 'react-native-idpass-smartshare' doesn't seem to be linked. Make sure: \n\n` +
+  `The package '@idpass/smartshare-react-native' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
