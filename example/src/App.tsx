fb0fa14ef94975d326cde57ac7b002666aebb29d
Arnelle Balane <arnelle@newlogic.com>
2022-02-16 15:40:45 +0800

    chore: fix linter errors in example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -21,27 +21,24 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import type { Node } from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import QRCode from 'react-native-qrcode-svg';
-import {MD5, isQRValid, getRandomSentence, getRandomString} from './Helper';
+import { MD5, isQRValid, getRandomString } from './Helper';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import { RNCamera } from 'react-native-camera';
 import BluetoothApi from '@idpass/smartshare-react-native';
 
-const Section = ({children, title}): Node => {
+const Section = ({ children, title }): Node => {
   const isDarkMode = false;
   return (
     <View style={styles.sectionContainer}>
@@ -51,7 +48,8 @@ const Section = ({children, title}): Node => {
           {
             color: isDarkMode ? Colors.white : Colors.black,
           },
-        ]}>
+        ]}
+      >
         {title}
       </Text>
       <Text
@@ -60,7 +58,8 @@ const Section = ({children, title}): Node => {
           {
             color: isDarkMode ? Colors.light : Colors.dark,
           },
-        ]}>
+        ]}
+      >
         {children}
       </Text>
     </View>
@@ -68,87 +67,87 @@ const Section = ({children, title}): Node => {
 };
 
 class App extends React.Component {
-
   constructor(props) {
-    super(props)
-    this.GetConnectionParameters = this.GetConnectionParameters.bind(this)
-    this.SetConnectionParameters = this.SetConnectionParameters.bind(this)
-    this.Disconnect = this.Disconnect.bind(this)
-    this.onScan = this.onScan.bind(this)
-    this.Send = this.Send.bind(this)
-    this.Discover = this.Discover.bind(this)
-    this.Advertise = this.Advertise.bind(this)
+    super(props);
+    this.GetConnectionParameters = this.GetConnectionParameters.bind(this);
+    this.SetConnectionParameters = this.SetConnectionParameters.bind(this);
+    this.Disconnect = this.Disconnect.bind(this);
+    this.onScan = this.onScan.bind(this);
+    this.Send = this.Send.bind(this);
+    this.Discover = this.Discover.bind(this);
+    this.Advertise = this.Advertise.bind(this);
 
     this.state = {
       page: 0,
-      msg: "",
-      checksum: "",
-      params: "hello",
+      msg: '',
+      checksum: '',
+      params: 'hello',
       color: 'gray',
-      logmsg: "",
+      logmsg: '',
       qrButtonDisabled: false,
       scanButtonDisabled: false,
       disconButtonDisabled: true,
       sendButtonDisabled: true,
       discoverButtonDisabled: false,
       advertiseButtonDisabled: false,
-      searchmsg: ""
-    }
-
+      searchmsg: '',
+    };
   }
 
   componentDidMount() {
     this.nearbyEvents = BluetoothApi.handleNearbyEvents((event) => {
-        console.log(event.type)
-        switch (event.type) {
-        case "msg":
-        var alphaOmega = event.data.substr(0, 20) + event.data.substr(event.data.length - 20);
-        this.setState({
-          msg: alphaOmega,
-          checksum: MD5(event.data)
-        });
-        break;
+      console.log(event.type);
+      switch (event.type) {
+        case 'msg':
+          var alphaOmega =
+            event.data.substr(0, 20) +
+            event.data.substr(event.data.length - 20);
+          this.setState({
+            msg: alphaOmega,
+            checksum: MD5(event.data),
+          });
+          break;
 
-        case "onDisconnected":
-        console.log("onDisconnected:" + event.data)
-        this.setState({
-          checksum: "",
-          msg: "",
-          params: "hello",
-          qrButtonDisabled: false,
-          scanButtonDisabled: false,
-          disconButtonDisabled: true,
-          sendButtonDisabled: true,
-          advertiseButtonDisabled: false,
-          discoverButtonDisabled: false,
-          searchmsg: ""
-        });
-        break;
+        case 'onDisconnected':
+          console.log('onDisconnected:' + event.data);
+          this.setState({
+            checksum: '',
+            msg: '',
+            params: 'hello',
+            qrButtonDisabled: false,
+            scanButtonDisabled: false,
+            disconButtonDisabled: true,
+            sendButtonDisabled: true,
+            advertiseButtonDisabled: false,
+            discoverButtonDisabled: false,
+            searchmsg: '',
+          });
+          break;
 
-        case "transferupdate":
-        console.log("transferupdate:" + event.data)
-        this.setState({
-          checksum: event.data,
-          sendButtonDisabled: event.data < 1.0 ? true:false
-        });
-        break;
+        case 'transferupdate':
+          console.log('transferupdate:' + event.data);
+          this.setState({
+            checksum: event.data,
+            sendButtonDisabled: event.data < 1.0 ? true : false,
+          });
+          break;
 
-        case "onEndpointFound":
-        console.log("found " + event.data)
-        break;
+        case 'onEndpointFound':
+          console.log('found ' + event.data);
+          break;
 
         default:
-        break;
+          break;
       }
-
-    })
+    });
 
     this.LogEvents = BluetoothApi.handleLogEvents((event) => {
-      var logmsg = (this.state.logmsg ? this.state.logmsg : "") + "\n" + event.log
+      var logmsg =
+        (this.state.logmsg ? this.state.logmsg : '') + '\n' + event.log;
       this.setState({
-        logmsg: logmsg
-      })
-    })
+        logmsg: logmsg,
+      });
+    });
   }
 
   componentWillUnmount() {
@@ -162,202 +161,234 @@ class App extends React.Component {
   }
 
   GetConnectionParameters() {
-    var params = JSON.parse(BluetoothApi.getConnectionParameters())
+    var params = JSON.parse(BluetoothApi.getConnectionParameters());
     this.setState({
       qrButtonDisabled: true,
       scanButtonDisabled: true,
-      msg: "",
-      checksum: "",
+      msg: '',
+      checksum: '',
       color: 'black',
       params: {
         cid: params.cid,
-        pk: params.pk
-      }
-    })
+        pk: params.pk,
+      },
+    });
 
-    BluetoothApi.createConnection("dual", () => {
-      console.log("-- connected ---")
+    BluetoothApi.createConnection('dual', () => {
+      console.log('-- connected ---');
       this.setState({
         page: 0,
-        params: "hello",
+        params: 'hello',
         color: 'gray',
         disconButtonDisabled: false,
-        sendButtonDisabled: false
-      })
-    })
+        sendButtonDisabled: false,
+      });
+    });
   }
 
   SetConnectionParameters() {
     this.setState({
       page: 1,
-      msg: "",
-      checksum: "",
+      msg: '',
+      checksum: '',
       qrButtonDisabled: true,
-      scanButtonDisabled: true
-    })
+      scanButtonDisabled: true,
+    });
   }
 
   onScan(qr) {
     if (!isQRValid(qr)) {
-      console.log("QR code is invalid");
+      console.log('QR code is invalid');
       this.setState({
         page: 0,
-        msg: "INVALID CONNECTION CODE",
+        msg: 'INVALID CONNECTION CODE',
         qrButtonDisabled: false,
-        scanButtonDisabled: false
-      })
-      return
+        scanButtonDisabled: false,
+      });
+      return;
     }
 
-    BluetoothApi.setConnectionParameters(qr.data)
-    BluetoothApi.createConnection("dual", () => {
-      console.log("-- connected ---")
+    BluetoothApi.setConnectionParameters(qr.data);
+    BluetoothApi.createConnection('dual', () => {
+      console.log('-- connected ---');
       this.setState({
         page: 0,
-        params: "hello",
+        params: 'hello',
         color: 'gray',
         disconButtonDisabled: false,
-        sendButtonDisabled: false
-      })
-    })
+        sendButtonDisabled: false,
+      });
+    });
 
     this.setState({
-      page: 0
-    })
+      page: 0,
+    });
   }
 
   Disconnect() {
-    console.log("*** Disconnect ***");
-    BluetoothApi.destroyConnection()
+    console.log('*** Disconnect ***');
+    BluetoothApi.destroyConnection();
 
     this.setState({
       qrButtonDisabled: false,
       scanButtonDisabled: false,
       disconButtonDisabled: true,
       sendButtonDisabled: true,
-      searchmsg: "",
+      searchmsg: '',
       advertiseButtonDisabled: false,
-      discoverButtonDisabled: false
-    })
+      discoverButtonDisabled: false,
+    });
   }
 
   Send() {
-    let msg = "alpha 9MB payload " + getRandomString(9000000) + " omega";
+    let msg = 'alpha 9MB payload ' + getRandomString(9000000) + ' omega';
     var alphaOmega = msg.substr(0, 20) + msg.substr(msg.length - 20);
 
     this.setState({
-      msg: "",
-      checksum: "",
-      sendButtonDisabled: true
+      msg: '',
+      checksum: '',
+      sendButtonDisabled: true,
     });
 
     BluetoothApi.send(msg, () => {
-      console.log("*** sent ***")
+      console.log('*** sent ***');
       this.setState({
         checksum: MD5(msg),
         msg: alphaOmega,
-        sendButtonDisabled: false
-      })
-    })
+        sendButtonDisabled: false,
+      });
+    });
   }
 
   Discover() {
-    var params = BluetoothApi.getConnectionParametersDebug()
-    BluetoothApi.setConnectionParameters(params)
+    var params = BluetoothApi.getConnectionParametersDebug();
+    BluetoothApi.setConnectionParameters(params);
 
     this.setState({
       advertiseButtonDisabled: true,
       discoverButtonDisabled: true,
-      searchmsg: "Searching ..."
-    })
+      searchmsg: 'Searching ...',
+    });
 
-    BluetoothApi.createConnection("discoverer", () => {
-      console.log("-- connected ---")
+    BluetoothApi.createConnection('discoverer', () => {
+      console.log('-- connected ---');
       this.setState({
         page: 0,
-        searchmsg: "i see you"
-      })
-    })
+        searchmsg: 'i see you',
+      });
+    });
   }
 
   Advertise() {
-    var params = BluetoothApi.getConnectionParametersDebug()
-    BluetoothApi.setConnectionParameters(params)
+    var params = BluetoothApi.getConnectionParametersDebug();
+    BluetoothApi.setConnectionParameters(params);
 
     this.setState({
       advertiseButtonDisabled: true,
       discoverButtonDisabled: true,
-      searchmsg: "Waiting to be found ..."
-    })
+      searchmsg: 'Waiting to be found ...',
+    });
 
-    BluetoothApi.createConnection("advertiser", () => {
-      console.log("-- connected ---")
+    BluetoothApi.createConnection('advertiser', () => {
+      console.log('-- connected ---');
       this.setState({
         page: 0,
-        searchmsg: "you see me!"
-      })
-    })
+        searchmsg: 'you see me!',
+      });
+    });
   }
 
   render() {
     switch (this.state.page) {
       case 0:
-      return (
-        <SafeAreaView style={{backgroundColor: Colors.white}} >
-          <ScrollView>
-            <View>
-              <Section title="example 0.0.1">
-              <Button title="QR" disabled={this.state.qrButtonDisabled} onPress={this.GetConnectionParameters} />
-              <View style={styles.space} />
-              <Button title="Scan" disabled={this.state.scanButtonDisabled} onPress={this.SetConnectionParameters} />
-              <View style={styles.space} />
-              <Button title="Discon" disabled={this.state.disconButtonDisabled} onPress={this.Disconnect} />
-              <View style={styles.space} />
-              <Button title="Send" disabled={this.state.sendButtonDisabled} onPress={this.Send} />
-              </Section>
-              <Section title="connection code">
-              <QRCode size={200} color={this.state.color} value={JSON.stringify(this.state.params)} />
-              </Section>
-              <Section title="visibility check">
-                <Button title="discover" disabled={this.state.discoverButtonDisabled} onPress={this.Discover} />
-                <View style={styles.space} />
-                <Button title="advertise" disabled={this.state.advertiseButtonDisabled} onPress={this.Advertise} />
-                <View style={styles.space} />
-                <Button title="DISCON" onPress={this.Disconnect} />
-              </Section>
-              <Section title="visibility result">
-                <Text>{this.state.searchmsg}</Text>
-              </Section>
-              <Section title="checksum">
-                <Text>{this.state.checksum}</Text>
-              </Section>
-              <Section title="message">
-                <Text>{this.state.msg}</Text>
-              </Section>
-              <Section title="log">
-                <Button title="Clear" onPress={() => {this.setState({logmsg:""})}} />
-                <Text>{this.state.logmsg}</Text>
-              </Section>
-            </View>
-          </ScrollView>
-        </SafeAreaView>
-      );
-      break;
+        return (
+          <SafeAreaView style={{ backgroundColor: Colors.white }}>
+            <ScrollView>
+              <View>
+                <Section title="example 0.0.1">
+                  <Button
+                    title="QR"
+                    disabled={this.state.qrButtonDisabled}
+                    onPress={this.GetConnectionParameters}
+                  />
+                  <View style={styles.space} />
+                  <Button
+                    title="Scan"
+                    disabled={this.state.scanButtonDisabled}
+                    onPress={this.SetConnectionParameters}
+                  />
+                  <View style={styles.space} />
+                  <Button
+                    title="Discon"
+                    disabled={this.state.disconButtonDisabled}
+                    onPress={this.Disconnect}
+                  />
+                  <View style={styles.space} />
+                  <Button
+                    title="Send"
+                    disabled={this.state.sendButtonDisabled}
+                    onPress={this.Send}
+                  />
+                </Section>
+                <Section title="connection code">
+                  <QRCode
+                    size={200}
+                    color={this.state.color}
+                    value={JSON.stringify(this.state.params)}
+                  />
+                </Section>
+                <Section title="visibility check">
+                  <Button
+                    title="discover"
+                    disabled={this.state.discoverButtonDisabled}
+                    onPress={this.Discover}
+                  />
+                  <View style={styles.space} />
+                  <Button
+                    title="advertise"
+                    disabled={this.state.advertiseButtonDisabled}
+                    onPress={this.Advertise}
+                  />
+                  <View style={styles.space} />
+                  <Button title="DISCON" onPress={this.Disconnect} />
+                </Section>
+                <Section title="visibility result">
+                  <Text>{this.state.searchmsg}</Text>
+                </Section>
+                <Section title="checksum">
+                  <Text>{this.state.checksum}</Text>
+                </Section>
+                <Section title="message">
+                  <Text>{this.state.msg}</Text>
+                </Section>
+                <Section title="log">
+                  <Button
+                    title="Clear"
+                    onPress={() => {
+                      this.setState({ logmsg: '' });
+                    }}
+                  />
+                  <Text>{this.state.logmsg}</Text>
+                </Section>
+              </View>
+            </ScrollView>
+          </SafeAreaView>
+        );
 
       case 1:
-      return(
-      <QRCodeScanner
-        onRead={this.onScan}
-        bottomContent={
-          <Text style={{fontWeight: "bold"}}>Scan QR Connection Code</Text>
-        }
-      />
-      );
-      break;
-
+        return (
+          <QRCodeScanner
+            onRead={this.onScan}
+            bottomContent={
+              <Text style={{ fontWeight: 'bold' }}>
+                Scan QR Connection Code
+              </Text>
+            }
+          />
+        );
     } // switch
   } // render
-};
+}
 
 const styles = StyleSheet.create({
   sectionContainer: {
@@ -378,18 +409,18 @@ const styles = StyleSheet.create({
   },
   space: {
     width: 20,
-    height: 20
+    height: 20,
   },
   whitebg: {
-    backgroundColor: '#FFFFFF'
+    backgroundColor: '#FFFFFF',
   },
   centerplace: {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     height: '100%',
-    backgroundColor: '#FFFFFF'
-  }
+    backgroundColor: '#FFFFFF',
+  },
 });
 
 export default App;
