c6199477299014d07f845bd6e5aacd1b55622441
Paolo Miguel de Leon <paolo@newlogic.com>
2022-07-28 17:41:14 +0800

    feat: added types

--- src/IdpassSmartshare.ts
+++ src/IdpassSmartshare.ts
@@ -14,7 +14,12 @@
  * limitations under the License.
  */
 
-import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
+import {
+  EmitterSubscription,
+  NativeEventEmitter,
+  NativeModules,
+  Platform,
+} from 'react-native';
 
 const LINKING_ERROR =
   `The package '@idpass/smartshare-react-native' doesn't seem to be linked. Make sure: \n\n` +
@@ -22,7 +27,7 @@ const LINKING_ERROR =
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
 
-const IdpassSmartshare = NativeModules.IdpassSmartshare
+const IdpassSmartshare: IdpassSmartshare = NativeModules.IdpassSmartshare
   ? NativeModules.IdpassSmartshare
   : new Proxy(
       {},
@@ -35,19 +40,66 @@ const IdpassSmartshare = NativeModules.IdpassSmartshare
 
 IdpassSmartshare.noop; // to trigger iOS autolink
 
-interface HandlerFunc {
-  (message: string): void;
-}
-
 if (Platform.OS === 'android') {
   const eventEmitter = new NativeEventEmitter();
-  IdpassSmartshare.handleNearbyEvents = (callback: HandlerFunc) =>
+  IdpassSmartshare.handleNearbyEvents = (callback) =>
     eventEmitter.addListener('EVENT_NEARBY', callback);
-  IdpassSmartshare.handleLogEvents = (callback: HandlerFunc) =>
+  IdpassSmartshare.handleLogEvents = (callback) =>
     eventEmitter.addListener('EVENT_LOG', callback);
 } else if (Platform.OS === 'ios') {
-  IdpassSmartshare.handleNearbyEvents = () => ({ remove: () => {} });
-  IdpassSmartshare.handleLogEvents = () => ({ remove: () => {} });
+  IdpassSmartshare.handleNearbyEvents = () =>
+    ({ remove: () => {} } as EmitterSubscription);
+  IdpassSmartshare.handleLogEvents = () =>
+    ({ remove: () => {} } as EmitterSubscription);
 }
 
 export default IdpassSmartshare;
+
+export interface IdpassSmartshare {
+  /**
+   * Do not invoke. Only used to trigger autolink in iOS builds.
+   */
+  noop: () => void;
+
+  getConnectionParameters: () => string;
+
+  setConnectionParameters: (params: string) => void;
+
+  getConnectionParametersDebug: () => string;
+
+  createConnection: (mode: ConnectionMode, callback: () => void) => void;
+
+  destroyConnection: () => void;
+
+  send: (message: string, callback: () => void) => void;
+
+  handleNearbyEvents: (
+    callback: (event: NearbyEvent) => void
+  ) => EmitterSubscription;
+
+  handleLogEvents: (
+    callback: (event: NearbyLog) => void
+  ) => EmitterSubscription;
+}
+
+export type ConnectionMode = 'dual' | 'advertiser' | 'discoverer';
+
+export type TransferUpdateStatus =
+  | 'SUCCESS'
+  | 'FAILURE'
+  | 'IN_PROGRESS'
+  | 'CANCELED';
+
+export type NearbyEvent =
+  | { type: 'msg'; data: string }
+  | { type: 'transferupdate'; data: TransferUpdateStatus }
+  | { type: 'onDisconnected'; data: string };
+
+export interface NearbyLog {
+  log: string;
+}
+
+export interface ConnectionParams {
+  cid: string;
+  pk: string;
+}
